69134bfd97c10a0d946b4e404937afd92d50f80c
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-08 16:19:45 +0545

    about confirm prompt and alert

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+// var cnfm = confirm("Are you 19...")
+// console.log(cnfm)
+
+// var alrt = alert("Its a warning!!!")
+// console.log(alrt)
+
+// var promppttt = prompt("Name...")
+// console.log(promppttt)
