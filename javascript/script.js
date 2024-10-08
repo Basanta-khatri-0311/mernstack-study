a91ef7f3261726c9eb085b2264693147af07689a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-08 22:41:13 +0545

    creating functions basic revision

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,15 @@
 
 // var promppttt = prompt("Name...")
 // console.log(promppttt)
+
+function walk(){
+    console.log("I am walking")
+}
+
+function sing(){
+    console.log("I am singing")
+}
+
+function dance(){
+    console.log("I am Dancing")
+}
