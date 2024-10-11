c24139888a4247540e0ef0d688b351f71d974fbc
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-11 14:59:50 +0545

    simple way of creating elements using react and reactdom

--- react/script.js
+++ react/script.js
@@ -1,5 +1,10 @@
-var h1 = document.createElement('h1')
-var root = document.getElementById('root')
-h1.innerHTML ='Hello from javascript'
-root.appendChild(h1)
+// var h1 = document.createElement('h1')
+// var root = document.getElementById('root')
+// h1.innerHTML ='Hello from javascript'
+// root.appendChild(h1)
+
+var h1 = React.createElement('h1',null,'Hello from React')
+var root = document.querySelector('#root')
+var renderer = ReactDOM.createRoot(root)
+renderer.render(h1)
 
