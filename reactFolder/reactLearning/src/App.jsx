0cc8ea2ccf06bd0140fecd4a3ab5a95500a09633
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-13 23:14:14 +0545

    creating a counter code

--- reactFolder/reactLearning/src/App.jsx
+++ reactFolder/reactLearning/src/App.jsx
@@ -12,17 +12,30 @@ import { useState } from "react";
 
 // export default app;
 
-function userDetails(){
-  const [user, setUSER] = useState('Basanta')
-  const changeName =()=>{
-    setUSER('Khatri')
-  }
+// function userDetails(){
+//   const [user, setUSER] = useState('Basanta')
+//   const changeName =()=>{
+//     setUSER('Khatri')
+//   }
+//   return(
+//     <div>
+//       <h1>Hello {user}</h1>
+//       <button onClick={changeName}>change user</button>
+//     </div>
+//   )
+// }
+
+// export default userDetails;
+
+function counter(){
+  const [value,setValue] = useState(0)
   return(
     <div>
-      <h1>Hello {user}</h1>
-      <button onClick={changeName}>change user</button>
+      <h1>Your number is {value}</h1>
+      <button onClick={()=>setValue(value-1)}>Increment</button>
+      <button onClick={()=>setValue(value+1)}>Decrement</button>
     </div>
   )
 }
 
-export default userDetails;
+export default counter;
