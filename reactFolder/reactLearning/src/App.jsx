17702532d2c8734f1ac91828f13e43a8fa075901
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-13 22:23:30 +0545

    creating and using variables in jsx

--- reactFolder/reactLearning/src/App.jsx
+++ reactFolder/reactLearning/src/App.jsx
@@ -1,5 +1,22 @@
-function  app(){
-  return <h1>Hello</h1>
+// function  app(){
+//   return <h1>Hello</h1>
+// }
+
+// export default app
+
+// function app() {
+//   return ( <h1>Hi from react app</h1> );
+// }
+
+// export default app;
+
+function userDetails(){
+  const user = 'Basanta'
+  return(
+    <div>
+      <h1>Hello {user}</h1>
+    </div>
+  )
 }
 
-export default app
+export default userDetails;
