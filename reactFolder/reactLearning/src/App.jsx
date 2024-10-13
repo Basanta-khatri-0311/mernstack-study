eaa296209b922617ab64fe870567bcea72d06355
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-13 23:24:21 +0545

    commenting

--- reactFolder/reactLearning/src/App.jsx
+++ reactFolder/reactLearning/src/App.jsx
@@ -27,15 +27,16 @@ import { useState } from "react";
 
 // export default userDetails;
 
-function counter(){
-  const [value,setValue] = useState(0)
-  return(
-    <div>
-      <h1>Your number is {value}</h1>
-      <button onClick={()=>setValue(value-1)}>Increment</button>
-      <button onClick={()=>setValue(value+1)}>Decrement</button>
-    </div>
-  )
-}
+// function counter(){
+//   const [value,setValue] = useState(0)
+//   return(
+//     <div>
+//       <h1>Your number is {value}</h1>
+//       <button onClick={()=>setValue(value-1)}>Increment</button>
+//       <button onClick={()=>setValue(value+1)}>Decrement</button>
+//     </div>
+//   )
+// }
 
-export default counter;
+// export default counter;
+
