fc2465ec82bde3eb9d70c7db092c4b29dd0bc0de
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-23 23:02:15 +0545

    update

--- reactFolder/reactLearning/src/components/Nav2.jsx
+++ reactFolder/reactLearning/src/components/Nav2.jsx
@@ -5,6 +5,7 @@ function NavTwo() {
       <h4 className="text-xl">About</h4>
       <h4 className="text-xl">Contact</h4>
       <h4 className="text-xl">Services</h4>
+      <h4 className="text-xl">Log Out</h4>
     </div>
   );
 }
